a341ae9d2833f264f0aee5855dec227f3b3b1e4a
IhorLev1 <lambermen1@gmail.com>
2020-12-13 17:37:17 +0200

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,6 @@ class App extends Component {
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
-    console.log(parsedContacts);
 
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
